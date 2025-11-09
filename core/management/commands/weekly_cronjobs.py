fe1620e31f75f92524e2519fe48037ca6ec3cb2e
Paul <paul@fraqment.de>
2025-11-09 18:28:54 +0100

    bugs fixed

--- core/management/commands/weekly_cronjobs.py
+++ core/management/commands/weekly_cronjobs.py
@@ -3,15 +3,22 @@ from django.contrib.auth.models import User
 from verbrauch.models import *
 from utils.functions import *
 
+from django.test import RequestFactory
+
+factory = RequestFactory()
+fake_request = factory.get('/')
+
 class Command(BaseCommand):
     help = "Weekly Cronjobs"
+
 
     def handle(self, *args, **options):
         def create_reminder():
             for user in User.objects.all():
                 try:
                     if not Aktion.objects.filter(user=user, date__gte=timezone.now().date() - timedelta(days=7)).exists():
-                        createBenachrichtigung(request=None, benachrichtigung=f"Hey {user.username}, du hast in der letzten Woche keine einzige Aktion eingetragen! Ändere das und schütze nicht nur das Klima, sondern steige auch in den Rankings deiner Families und Communities sowie in deinem Level auf!", user=user)
+                        fake_request = fake_request.user = user
+                        createBenachrichtigung(fake_request, benachrichtigung=f"Hey {user.username}, du hast in der letzten Woche keine einzige Aktion eingetragen! Ändere das und schütze nicht nur das Klima, sondern steige auch in den Rankings deiner Families und Communities sowie in deinem Level auf!", user=user)
                 except Exception as e:
                     error_message = f'Cronjob: {timezone.now().isoformat()} Create Reminder for User: {user}: Exception: {e}'
                     with open('logs/errors.log', 'a') as file:
