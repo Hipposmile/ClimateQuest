3f5013fd6479158d4389f89fe75a080d034299aa
Paul <paul@fraqment.de>
2025-12-20 09:51:32 +0100

    tryy

--- templates/sw.js
+++ templates/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+/*importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
 
@@ -41,7 +41,7 @@ self.addEventListener('fetch', (event) => {
       }
     })());
   }
-});
+});*/
 
 self.addEventListener('push', function (event) {
     console.log("receiving push")
