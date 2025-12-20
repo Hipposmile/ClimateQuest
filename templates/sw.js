594a92cb0f959d38ee0aeaacdb8ec2576cb5bb58
Paul <paul@fraqment.de>
2025-12-20 09:48:23 +0100

    try

--- templates/sw.js
+++ templates/sw.js
@@ -1,5 +1,3 @@
-// This is the "Offline page" service worker
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
@@ -45,15 +43,14 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
-// Register event listener for the 'push' event.
 self.addEventListener('push', function (event) {
-    // Retrieve the textual payload from event.data (a PushMessageData object).
-    // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
-    // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+    console.log("receiving push")
     const eventInfo = event.data.text();
     const data = JSON.parse(eventInfo);
     const head = data.head || 'New Notification 🕺🕺';
     const body = data.body || 'This is default content. Your notification didn\'t have one 🙄🙄';
+
+    console.log(`received data: ${head}, ${body}`)
 
     // Keep the service worker alive until the notification is created.
     event.waitUntil(
