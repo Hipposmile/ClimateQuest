1871dd13934a16b30ca2d768aec1ddfbb8fcf134
Paul <paul@fraqment.de>
2026-01-29 13:14:56 +0100

    bugs fixed

--- core/middleware.py
+++ core/middleware.py
@@ -31,18 +31,18 @@ class GenerateCSPNonceMiddleware:
     def __call__(self, request):
         script_nonce = base64.b64encode(os.urandom(16)).decode()
         request.csp_script_nonce = script_nonce
-        allowed_script_sources = 'https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js'
+        allowed_script_sources = 'https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://storage.googleapis.com/workbox-cdn/releases/5.1.2/'
 
         style_nonce = base64.b64encode(os.urandom(16)).decode()
         request.csp_style_nonce = style_nonce
-        allowed_style_sources = 'https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css'
+        allowed_style_sources = 'https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css https://www.gstatic.com/recaptcha/'
 
         response = self.get_response(request)
 
         csp = (
             f"default-src 'self'; "
-            f"script-src 'self' 'nonce-{script_nonce}' {allowed_script_sources} https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/; "
-            f"style-src 'self' 'nonce-{style_nonce}' {allowed_style_sources} https://www.gstatic.com/recaptcha/; "
+            f"script-src 'self' 'nonce-{script_nonce}' {allowed_script_sources}; "
+            f"style-src 'self' 'nonce-{style_nonce}' {allowed_style_sources}; "
             f"img-src 'self' data: https://www.gstatic.com/recaptcha/; "
             f"frame-src https://www.google.com/recaptcha/; "
             f"connect-src 'self' https://www.google.com/recaptcha/;"
