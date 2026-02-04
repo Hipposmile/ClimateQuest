d1ff349d60f9d1a15cceb2a80067ff1351e5409a
Paul <paul@fraqment.de>
2026-02-04 20:52:58 +0100

    added aknachhaltigkeit page

--- pages/urls.py
+++ pages/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('testers/', testers, name='testers'),
+    path('aknachhaltigkeit/', aknachhaltigkeit, name='aknachhaltigkeit')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def testers(request):
     return render(request, './testers.html')
+
+def aknachhaltigkeit(request):
+    return render(request, './aknachhaltigkeit.html')
