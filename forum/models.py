c3e46da12184bae910d71eec93bd5ead7f5de391
Paul <paul@fraqment.de>
2026-04-13 20:00:53 +0200

    added model category to forum/models.py

--- forum/models.py
+++ forum/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
 class ForumPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -11,6 +19,7 @@ class ForumPost(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Answer(models.Model):
     content = models.TextField()
