c3eee0d4cb8b63f919fbd5e0257c41d07ff88aa8
Paul <paul@fraqment.de>
2026-04-13 20:01:41 +0200

    added category to admin (forum)

--- forum/admin.py
+++ forum/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import ForumPost, Answer
+from .models import ForumPost, Answer, Category
+
 # Register your models here.
 admin.site.register(ForumPost)
 admin.site.register(Answer)
+admin.site.register(Category)
