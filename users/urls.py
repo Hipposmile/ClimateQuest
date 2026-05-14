7b6db9c1be2355929831ae90bf439438e14a00d4
Paul <paul@fraqment.de>
2026-05-14 18:37:54 +0200

    added / behind urls in users

--- users/urls.py
+++ users/urls.py
@@ -4,13 +4,13 @@ from .views import klimapunkte_view, klimapunkte_me, level_view, level_me, histo
     user_detail, action_detail
 
 urlpatterns = [
-    path('klimapunkte/<int:user_id>', klimapunkte_view, name='klimapunkte_view'),
-path('klimapunkte/me', klimapunkte_me, name='klimapunkte_me'),
-    path('level/<int:user_id>', level_view, name='level_view'),
-    path('level/me', level_me, name='level_me'),
-    path('history/<int:user_id>', history, name='history'),
-    path('action_detail/<int:action_id>/<int:user_id>', action_detail, name='action_detail'),
-    path('history/me', history_me, name='history_me'),
+    path('klimapunkte/<int:user_id>/', klimapunkte_view, name='klimapunkte_view'),
+    path('klimapunkte/me/', klimapunkte_me, name='klimapunkte_me'),
+    path('level/<int:user_id>/', level_view, name='level_view'),
+    path('level/me/', level_me, name='level_me'),
+    path('history/<int:user_id>/', history, name='history'),
+    path('action_detail/<int:action_id>/<int:user_id>/', action_detail, name='action_detail'),
+    path('history/me/', history_me, name='history_me'),
     path('overview/', users_overview, name='users_overview'),
-    path('detail/<int:user_id>', user_detail, name='user_detail')
+    path('detail/<int:user_id>/', user_detail, name='user_detail')
 ]
