f8fcc1443a692701b2b7c7c51bb9477a99436ad1
Paul <paul@fraqment.de>
2026-05-20 19:45:40 +0200

    fixed warning from allauth, edited hall_of_fame_entry.created at --> now no time

--- hall_of_fame/models.py
+++ hall_of_fame/models.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.utils import timezone
 
 # Create your models here.
 class HallOfFameEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hall_of_fame_entry')
     description_de = models.TextField()
     description_en = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(default=timezone.now)
 
     def __str__(self):
         return f"{self.user.username} --> {self.description_de}"
