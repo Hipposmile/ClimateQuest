fa8dc4df6220c95de780f85b866b5e89c3f98511
Paul <paul@fraqment.de>
2026-05-22 19:50:51 +0200

    bug fixed

--- personals/signals.py
+++ personals/signals.py
@@ -4,11 +4,6 @@ from django.dispatch import receiver
 from .adapters import MySocialAccountAdapter
 from .views import actions_after_registration
 
-@receiver(social_account_updated)
-def sync_google_data(sender, request, sociallogin, **kwargs):
-    adapter = MySocialAccountAdapter()
-    adapter.sync_google_data(request, sociallogin)
-
 @receiver(user_signed_up)
 def create_user_profile(sender, request, user, **kwargs):
     actions_after_registration(request, user)
