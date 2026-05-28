fbc93c97d486879c64840380f415a80228610ca7
Paul <paul@fraqment.de>
2026-05-28 09:15:23 +0200

    rm try

--- ClimateQuest/base_settings.py
+++ ClimateQuest/base_settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-#from django.conf.global_settings import APPEND_SLASH
+from django.conf.global_settings import APPEND_SLASH
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -220,4 +220,4 @@ APPLE_BUNDLE_ID = os.environ.get("APPLE_BUNDLE_ID")
 # Entwicklung/Sandbox (für Development-Zertifikate)
 APNS_HOST = "https://api.sandbox.push.apple.com"
 
-APPEND_SLASH = False
+#APPEND_SLASH = False
